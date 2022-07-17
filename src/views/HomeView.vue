aeb3b110accdd72ffe0216e514fc8c4c6c27d7a5
unknown <caryhuit@gmail.com>
2022-07-17 09:26:30 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+<html>
+<body>
+
+  <h1>CARY HUIT</h1>
+  <h1>BSIT 3</h1>
+  <h2>CAWAYANAN, TUBIGON, BOHOL</h2>
+  <h2>09876543212</h2>
+
+</body>
+</html>
+</template>
+<style>
+h1 {text-align: center;font-size: 100px; font-family: 'Times New Roman', Times, serif;}
+h2 {text-align: center;font-size: 75px;}
+</style>
